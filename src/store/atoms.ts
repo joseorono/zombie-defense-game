19420599ef291f3e40b6d3daa1e4724fbe854f51
José Miguel Oroño <joseomaker@gmail.com>
2024-09-27 23:07:24 -0500

    Create atoms.ts

---
+++ src/store/atoms.ts
@@ -0,0 +1,57 @@
+import { atomWithStorage } from 'jotai/utils';
+import { withImmer } from 'jotai-immer';
+import { atom } from 'jotai';
+
+import { eventData, IPlayerWeaponStats, playerResources, ScoreEntry, unitCount } from '~/types/game-data-types';
+import { IModal } from '~/types/dialog-props';
+import { NOTHING_EVENT } from '~/constants/events';
+import { getRandomRomanName, getRandomRomanTownName } from '~/lib/utils';
+
+// Audio atoms
+export const isMutedAtom = atomWithStorage<boolean>('isMuted', false);
+export const volumeAtom = atomWithStorage<number>('volume', 1);
+
+//player atoms
+export const playerNameAtom = atomWithStorage<string>('playerName', getRandomRomanName());
+export const townNameAtom = atomWithStorage<string>('townName', getRandomRomanTownName());
+export const civilizationLevelAtom = atomWithStorage<number>('civLevel', 0);
+export const showTutorialsAtom = atomWithStorage<boolean>('showTutorials', true);
+export const playerWeaponAtom = atomWithStorage<IPlayerWeaponStats>('playerWeapon', {
+  damage: 1,
+  attackSpeed: 1,
+  ammoCapacity: 1,
+  reloadSpeed: 1,
+});
+
+// Atoms for the modal
+export const isModalOpenAtom = atom<boolean>(false);
+export const modalContentAtom = atom<IModal>({ title: '', subtitle: '', content: null, onClose: () => {} });
+export const setContentAtom = atom(null, (_, set, newContent: IModal) => {
+  //when you set new content, show modal immediately
+  set(modalContentAtom, newContent);
+  set(isModalOpenAtom, true);
+});
+
+export const unitsAtom = withImmer(
+  atomWithStorage<unitCount>('units', {
+    gunman: 1,
+    shotgunner: 0,
+    barricade: 2,
+    'barbed-barricade': 0,
+    mine: 0,
+    tripwire: 0,
+  }),
+);
+
+export const resourcesAtom = withImmer(
+  atomWithStorage<playerResources>('resources', {
+    food: 0,
+    water: 0,
+    wood: 0,
+    scrap: 0,
+  }),
+);
+
+export const scoreboardAtom = atomWithStorage<ScoreEntry[]>('score', []);
+
+export const currentEventAtom = withImmer(atomWithStorage<eventData>('event', NOTHING_EVENT));

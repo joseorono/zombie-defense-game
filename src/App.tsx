aa5b622437d8c40744320e232828c34f122b9cf1
José Miguel Oroño <joseomaker@gmail.com>
2024-09-28 03:27:41 -0500

    Clean up App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Toaster } from 'react-hot-toast';
+//import { Repeat, Delay } from 'timeline-composer';
+import '~/index.css';
+
+import { useEffect } from 'react';
+import MouseTracker from '~/components/effects/mouse-tracker';
+import DebugArea from '~/components/debug-area';
+
+import ResponsiveNotice from '~/components/responsive-notice';
+import BackgroundWorker from '~/components/game/background-worker';
+import Modal from '~/components/Modal';
+import GameScreen from '~/components/game/game-screen';
+import GameLoader from '~/components/game/game-loader';
+import { GAME_TICK_SECONDS } from './constants/defaults';
+import TailwindDemo from './components/demo/tailwind-demo';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const toastClasses = 'pixel-rounded font-bold text-xl mx-2 w-11/12 md:w-fit min-w-[200px]';
+
+  useEffect(() => {
+    const handler = (e: Event) => e.preventDefault();
+    document.addEventListener('gesturestart', handler);
+    document.addEventListener('gesturechange', handler);
+    document.addEventListener('gestureend', handler);
+    return () => {
+      document.removeEventListener('gesturestart', handler);
+      document.removeEventListener('gesturechange', handler);
+      document.removeEventListener('gestureend', handler);
+    };
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <TailwindDemo />
+      <MouseTracker />
+      <ResponsiveNotice />
+      {/*
+      <Delay seconds={1}>
+        <Repeat seconds={GAME_TICK_SECONDS}>
+          <BackgroundWorker />
+        </Repeat>
+      </Delay>
+    */}
+      <Modal />
+
+      {/* <DebugArea /> */}
+
+      {/* 
+    
+
+      <GameLoader />
+      
+      */}
+
+      <Toaster
+        gutter={12}
+        toastOptions={{
+          position: 'bottom-center',
+
+          // Aria
+          ariaProps: {
+            role: 'status',
+            'aria-live': 'polite',
+          },
+
+          // Styling
+          className: toastClasses,
+
+          success: {
+            style: {
+              background: '#4BB543',
+              color: '#fff',
+            },
+          },
+          error: {
+            style: {
+              background: '#d32f2f',
+              color: '#fff',
+            },
+          },
+          custom: {
+            className: toastClasses,
+            style: {
+              background: '#333',
+              color: '#fff',
+              textShadow: '1px 1px 2px #000',
+            },
+          },
+        }}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

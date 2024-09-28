e89ebcc9ba404d63008fd2a0bf873a74177a3cd0
José Miguel Oroño <joseomaker@gmail.com>
2024-09-28 02:48:55 -0500

    Improve getRandomEvent

--- src/lib/events-logic.ts
+++ src/lib/events-logic.ts
@@ -1,26 +1,30 @@
 import { eventData } from '~/types/game-data-types';
+import { getRandomElement } from './utils';
 
-export function getRandomEvent(EVENTS: Array<eventData>): eventData {
-  if (!EVENTS) {
+export function getRandomEvent(events: Array<eventData>, isWeighted: boolean = false): eventData {
+  // We can receive events as an array with little performance cost because they're passed by reference
+  if (!events) {
     throw new Error('EMPTY_EVENTS: No events passed.');
   }
 
-  // Calculate the sum of all probabilities
-  const totalWeight = EVENTS.reduce((acc, event) => acc + event.probability, 0);
+  if (isWeighted) {
+    // Calculate the sum of all probabilities
+    const totalWeight = events.reduce((acc, event) => acc + event.probability, 0);
 
-  // Generate a random number between 0 and totalWeight
-  // This will be used to select a random event
-  let random = Math.random() * totalWeight;
+    // Generate a random number between 0 and totalWeight
+    // This will be used to select a random event
+    let random = Math.random() * totalWeight;
 
-  // Iterate through the events and select one based on the random number
-  for (let event of EVENTS) {
-    if (random < event.probability) {
-      return event;
+    // Now use the weight of each event to determine which event to select
+    for (const event of events) {
+      random -= event.probability;
+      if (random < 0) {
+        return event;
+      }
     }
-    random -= event.probability;
   }
 
-  // In case something goes wrong, return the first one
+  // In case something goes wrong, return a random one without accounting for weights
   // This technically should never happen, but this failsafe has no runtime cost
-  return EVENTS[0];
+  return getRandomElement(events);
 }

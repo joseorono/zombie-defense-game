1929854b50d620d73170efb4b8f488c6d1d230d0
José Miguel Oroño <joseomaker@gmail.com>
2024-09-27 22:50:03 -0500

    Create game-data-types.ts

---
+++ src/types/game-data-types.ts
@@ -0,0 +1,179 @@
+/*
+======================================
+        Game Data Types
+======================================
+*/
+
+export type playerResources = {
+  food: number;
+  water: number;
+  wood: number;
+  scrap: number;
+};
+
+export type priceData = {
+  costFood: number;
+  costWater: number;
+  costWood: number;
+  costScrap: number;
+};
+
+export interface IPlayerWeaponStats {
+  damage: number;
+  attackSpeed: number;
+  ammoCapacity: number;
+  reloadSpeed: number;
+}
+
+/*
+======================================
+        Scoreboard
+======================================
+*/
+
+export type ScoreEntry = {
+  playerName: string;
+
+  // Counters
+  civilianAdultCount: number;
+  civilianChildCount: number;
+  daysSurvived: number;
+  unitsRemaining: number;
+
+  // Computed Values
+  pointsFromdays: number;
+  pointsFromCivilians: number;
+  pointsFromWeaponUpgrades: number;
+  pointsFromResources: number;
+  totalScore: number;
+};
+
+/*
+======================================
+        Event
+======================================
+*/
+
+export type EventOutcome = {
+  civiliansDelta: number;
+  remainingdaysDelta: number; // For making the game last longer or less
+} & playerResources;
+
+/*
+======================================
+        Tower Defense Minigame
+======================================
+*/
+
+type enemyType = 'basic' | 'fast' | 'strong' | 'dog' | 'boss';
+
+/*
+    Gunman: Shoots enemies in row.
+    Shotgunner: Shoots enemies in a cone or just hits harder but is slower.
+    Barricade: Stops enemies until it breaks.
+    Barbed Barricade: Stops enemies and deals damage, more fragile than normal barricades.
+    Mine: Explodes when enemies step on it, killing one enemy.
+    Tripwire: Detonates and kills all enemies in row.
+*/
+export type unitTypes = 'gunman' | 'shotgunner' | 'barricade' | 'barbed-barricade' | 'mine' | 'tripwire';
+
+// If done this way, we can have a base class for all units, using different stats for each unit.
+// Maybe this would work better as a class?
+//export interface IUnitStats {}
+/*
+class baseUnit {
+  hp: number;
+  damageOnContact: number;
+  damageRanged: number;
+  range: number;
+  killsRow: boolean;
+  attackSpeed: number;
+  ammoCapacity: number;
+  reloadSpeed: number;
+  cost: playerResources;
+}
+*/
+
+export type unitCount = {
+  [key in unitTypes]: number;
+};
+
+export type enemyData = {
+  hp: number;
+  damage: number;
+  moveSpeed: number;
+  attackSpeed: number;
+  reward: Nullable<playerResources>;
+};
+
+export type enemyCount = {
+  [key in enemyType]: number;
+};
+
+export type IDefenseWaveProps = {
+  waveNumber: number;
+  enemies: enemyCount;
+  durationInSeconds: number;
+  units: unitCount;
+};
+
+export interface IDefenseGameResults {
+  reward: playerResources;
+  enemiesKilled: enemyCount;
+  unitsLost: unitCount;
+  civiliansLost: number;
+  daysDelayed: number;
+}
+
+/* 
+======================================
+            Places 
+======================================
+*/
+
+export type placeData = {
+  id: number;
+  name: string;
+  description: string;
+  dailyIncome: playerResources;
+};
+
+export type placeKeys = 'water' | 'military-base' | 'land' | 'gunshop' | 'grocery' | 'house';
+
+export type buildingMap = {
+  [key in placeKeys]: placeData;
+};
+
+/* 
+======================================
+            Civilians 
+======================================
+*/
+
+export type genders = 'male' | 'female';
+export type ages = 'child' | 'adult';
+
+export type civilianData = {
+  id: number;
+  name: string;
+  gender: genders;
+  age: ages;
+};
+
+/* 
+======================================
+        Upgrades   
+======================================
+*/
+
+export type civLevelData = {
+  baseMultiplier: number;
+} & priceData;
+
+export type civLevelKeys = 'wasteland' | 'tribalSociety' | 'chiefdom' | 'republic';
+
+export type upgradeMap = Record<civLevelKeys, civLevelData>;
+
+export type upgradeCount = {
+  [key in civLevelKeys]: number;
+};

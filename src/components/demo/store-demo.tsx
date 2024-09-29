1a663e3c44787728cb3cab9e1bc7ccf5d78de5d3
José Miguel Oroño <joseomaker@gmail.com>
2024-09-29 03:07:28 -0500

    Create store-demo.tsx

---
+++ src/components/demo/store-demo.tsx
@@ -0,0 +1,9 @@
+const nameArray = ['Lucius', 'Augustus', 'Aurelius', 'Iosephus', 'Carolus', 'Eduardus'];
+//import { useAtom, useAtomValue } from 'jotai';
+//import { resourcesAtom, playerNameAtom, civilizationLevelAtom } from '~/store/atoms';
+
+function StoreDemo() {
+  return 'Store Demo';
+}
+
+export default StoreDemo;

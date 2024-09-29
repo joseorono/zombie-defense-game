f437d6b023d642384debcbec52e206c041fc555d
José Miguel Oroño <joseomaker@gmail.com>
2024-09-29 03:05:56 -0500

    Update credits-dialog.tsx

--- src/components/modals/credits-dialog.tsx
+++ src/components/modals/credits-dialog.tsx
@@ -92,12 +92,7 @@ export default function CreditsDialog() {
           <h3 className="text-xl" id="sound-effects-">
             Sound Effects:
           </h3>
-          <p>
-            Whip Crack, used when buy a slave. Creative Commons 0, by JayRom01&nbsp;
-            <a href="https://freesound.org/people/JayRom01/sounds/615761/">
-              https://freesound.org/people/JayRom01/sounds/615761/
-            </a>
-          </p>
+
           <p>
             Close modal sound, Creative Commons 0, modusmogulus&nbsp;
             <a href="https://freesound.org/people/modusmogulus/sounds/735897/">
@@ -110,90 +105,25 @@ export default function CreditsDialog() {
               https://freesound.org/people/BiORNADE/sounds/735804/
             </a>
           </p>
-          <p>
-            Pickaxe, for miners and quarry, pickaxe 1/4 CC Attribution 4.0, TechspiredMinds&nbsp;
-            <a href="https://freesound.org/people/TechspiredMinds/sounds/728756/">
-              https://freesound.org/people/TechspiredMinds/sounds/728756/
-            </a>
-          </p>
-          <p>
-            chicken sound for farmers and farm 01130 chicken hen 2.wav CC Attribution 4.0, Robinhood76&nbsp;
-            <a href="https://freesound.org/people/Robinhood76/sounds/75726/">
-              https://freesound.org/people/Robinhood76/sounds/75726/
-            </a>
-          </p>
-          <p>
-            fire crackling for bakers and bakery Fire_Crackle_2.wav Creative Commons 0, KieranKeegan&nbsp;
-            <a href="https://freesound.org/people/KieranKeegan/sounds/422741/">
-              https://freesound.org/people/KieranKeegan/sounds/422741/
-            </a>
-          </p>
+
           <p>
             coin pouch for mercator Pouch of Gold Coins, Rummaging Through.wav, Creative Commons 0, The_Frisbee_of_Peace
             &nbsp;
-            {/* <a href="https://freesound.org/people/The_Frisbee_of_Peace/sounds/575576/">
+            <a href="https://freesound.org/people/The_Frisbee_of_Peace/sounds/575576/">
               https://freesound.org/people/The_Frisbee_of_Peace/sounds/575576/
-            </a> */}
-          </p>
-          <p>
-            blacksmith hammer for smithy and blacksmith Blacksmith Hammer, Creative Commons 0, Kastenfrosch&nbsp;
-            <a href="https://freesound.org/people/Kastenfrosch/sounds/521972/">
-              https://freesound.org/people/Kastenfrosch/sounds/521972/
             </a>
           </p>
-          <p>
-            Marching, for legionary and castra Marching 3.wav CC Attribution 4.0, WebbFilmsUK&nbsp;
-            <a href="https://freesound.org/people/WebbFilmsUK/sounds/200322/">
-              https://freesound.org/people/WebbFilmsUK/sounds/200322/
-            </a>
-          </p>
-          <p>
-            Amen for priests and temple, amen.wav, CC Attribution 4.0, ERH&nbsp;
-            <a href="https://freesound.org/people/ERH/sounds/29933/">https://freesound.org/people/ERH/sounds/29933/</a>
-          </p>
-          <p>
-            Crowd for forum Crowd.wav CC Attribution 4.0, Haykrich&nbsp;
-            <a href="https://freesound.org/people/Haykrich/sounds/401560/">
-              https://freesound.org/people/Haykrich/sounds/401560/
-            </a>
-          </p>
-          <p>
-            grass for fields Rustling Grass 4.wav, Creative Commons 0, morganpurkis&nbsp;
-            <a href="https://freesound.org/people/morganpurkis/sounds/396016/">
-              https://freesound.org/people/morganpurkis/sounds/396016/
-            </a>
-          </p>
-          <p>
-            level up sound Medieval Fanfare.aiff, CC Attribution 3, tobyk&nbsp;
-            <a href="https://freesound.org/people/tobyk/sounds/26198/">
-              https://freesound.org/people/tobyk/sounds/26198/
-            </a>
-          </p>
-          <p>
-            game over sound 02362 fanfare announce a.wav CC Attribution 4.0, Robinhood76&nbsp;
-            <a href="https://freesound.org/people/Robinhood76/sounds/123435/">
-              https://freesound.org/people/Robinhood76/sounds/123435/
-            </a>
-          </p>
-          <h3 className="text-xl" id="fanfares-sound-to-play-when-the-play-button-is-clicked-before-game-starts-">
-            Fanfares (Sound to Play when the PLAY button is clicked, before game starts)
-          </h3>
-          <p>
-            Fanfare, for playing when a New Game starts, CC Attribution 4.0, zagi2&nbsp;
-            <a href="https://freesound.org/people/zagi2/sounds/193934/">
-              https://freesound.org/people/zagi2/sounds/193934/
-            </a>
-          </p>
+
           <h3 className="text-xl" id="music-">
             MUSIC:
           </h3>
-          <p>
-            Background Music: "An Arcadian sound, an ancient harpist, a mythological atmosphere." Attribution
-            NonCommercial 4.0&nbsp;
-            <a href="https://freesound.org/people/cormi/sounds/169035/">
-              https://freesound.org/people/cormi/sounds/169035/
-            </a>
-          </p>
+          <p>TBD</p>
+
+          {/* 
+            =================================
+                    Licenses 
+            =================================
+        */}
           <h3 className="text-xl" id="links-to-licences-">
             LINKS TO LICENCES:
           </h3>

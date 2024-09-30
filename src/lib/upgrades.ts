63ef028b48af3feba8c64ad5326154c5dcc9dc3a
José Miguel Oroño <joseomaker@gmail.com>
2024-09-30 00:45:00 -0500

    Update upgrades.ts

--- src/lib/upgrades.ts
+++ src/lib/upgrades.ts
@@ -1,16 +1,16 @@
-import { LEVELS, LEVELS_COUNT } from '~/constants/upgrades';
+import { CIV_LEVELS, CIV_LEVELS_COUNT } from '~/constants/upgrades';
 import { playerResources } from '~/types/game-data-types';
 
 export function canAffordNextLevel(currentLevel: number, res: playerResources): boolean {
   // Exit early
-  if (currentLevel + 1 >= LEVELS_COUNT) {
+  if (currentLevel + 1 >= CIV_LEVELS_COUNT) {
     return false;
   }
 
   if (
-    res.gold >= LEVELS[currentLevel + 1].costGold &&
-    res.grain >= LEVELS[currentLevel + 1].costGrain &&
-    res.stone >= LEVELS[currentLevel + 1].costStone
+    res.gold >= CIV_LEVELS[currentLevel + 1]. &&
+    res.grain >= CIV_LEVELS[currentLevel + 1].costGrain &&
+    res.stone >= CIV_LEVELS[currentLevel + 1].costStone
   ) {
     return true;
   }

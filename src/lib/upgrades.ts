80a30a026c0988081fc65ae123787b791378eab3
José Miguel Oroño <joseomaker@gmail.com>
2024-09-30 00:55:41 -0500

    Add new update management functions

--- src/lib/upgrades.ts
+++ src/lib/upgrades.ts
@@ -1,5 +1,5 @@
 import { CIV_LEVELS, CIV_LEVELS_COUNT } from '~/constants/upgrades';
-import { playerResources } from '~/types/game-data-types';
+import { civLevelData, playerResources } from '~/types/game-data-types';
 
 export function canAffordNextLevel(currentLevel: number, res: playerResources): boolean {
   // Exit early
@@ -7,13 +7,24 @@ export function canAffordNextLevel(currentLevel: number, res: playerResources): 
     return false;
   }
 
-  if (
-    res.gold >= CIV_LEVELS[currentLevel + 1]. &&
-    res.grain >= CIV_LEVELS[currentLevel + 1].costGrain &&
-    res.stone >= CIV_LEVELS[currentLevel + 1].costStone
-  ) {
-    return true;
+  return (
+    res.food >= CIV_LEVELS[currentLevel + 1].foodCost &&
+    res.water >= CIV_LEVELS[currentLevel + 1].waterCost &&
+    res.wood >= CIV_LEVELS[currentLevel + 1].woodCost &&
+    res.scrap >= CIV_LEVELS[currentLevel + 1].scrapCost
+  );
+}
+
+export function isThereNextLevel(currentLevel: number): boolean {
+  return currentLevel + 1 < CIV_LEVELS_COUNT;
+}
+
+export function getNextLevelData(currentLevel: number): civLevelData {
+  // Check that we're not at the end of the levels
+  if (currentLevel + 1 > CIV_LEVELS_COUNT) {
+    console.error('Trying to get next level data when we are at the end of the levels');
+    throw new Error('Trying to get next level data when we are at the end of the levels');
   }
 
-  return false;
+  return CIV_LEVELS[currentLevel + 1];
 }
